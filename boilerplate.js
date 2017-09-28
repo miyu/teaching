5a940dcceb4da34c2e040110199fbe75e3d29baa
Warty <ItzWarty@gmail.com>
2017-09-28 11:52:07 -0700

    Platformer hacked together

--- boilerplate.js
+++ boilerplate.js
@@ -5,6 +5,10 @@ global.canvasHeight = -1;
 global.context = null;
 global.frameEnterHooks = [];
 global.lastRenderTime = new Date();
+global.startTime = new Date();
+global.tickRate = -1;
+global.ticksExecuted = 0;
+global.tickEnterHooks = [];
 
 // ArrowLeft, ArrowRight, ArrowUp, ArrowDown
 global.Key = {
@@ -13,32 +17,46 @@ global.Key = {
     }
 };
 
-function init(width, height) {
+function init(width, height, tickRate) {
     if (global.canvas) {
         throw "Cannot call initDisplay multiple times!";
     }
 
-    document.addEventListener('keydown', (evt) => { global.Key[evt.key.toLowerCase()] = true; });
-    document.addEventListener('keyup', (evt) => { global.Key[evt.key.toLowerCase()] = false; });
+    document.addEventListener('keydown', (evt) => { global.Key[evt.keyCode] = global.Key[evt.key.toLowerCase()] = global.Key[evt.code.toLowerCase()] = true; });
+    document.addEventListener('keyup', (evt) => { global.Key[evt.keyCode] = global.Key[evt.key.toLowerCase()] = global.Key[evt.code.toLowerCase()] = false; });
 
     global.canvas = document.createElement('canvas');
     global.canvasWidth = global.canvas.width = width;
     global.canvasHeight = global.canvas.height = height;
     global.context = canvas.getContext('2d');
     document.body.appendChild(canvas);
+    global.tickRate = tickRate || 60;
     window.requestAnimationFrame(renderStep);
 }
 
 function renderStep() {
     var now = new Date();
     var dt = (now - global.lastRenderTime) / 1000;
+    var t = (now - global.startTime) / 1000;
+    var lastT = (global.lastRenderTime - global.startTime) / 1000;
+
+    const ticksDesired = Math.floor(t * global.tickRate);
+    for (let i = 0; global.ticksExecuted < ticksDesired; i++, global.ticksExecuted++) {
+        const dt = 1 / global.tickRate;
+        tickEnterHooks.forEach(hook => hook(dt, lastT + i * dt));
+    }
+
     global.lastRenderTime = now;
-    frameEnterHooks.forEach(hook => hook(dt));
+    frameEnterHooks.forEach(hook => hook(dt, t));
     window.requestAnimationFrame(renderStep);
 }
 
 function onFrameEnter(handler) {
     global.frameEnterHooks.push(handler);
+}
+
+function onTickEnter(handler) {
+    global.tickEnterHooks.push(handler);
 }
 
 function assert(val, msg) {
@@ -77,6 +95,18 @@ function fillRect() {
             : Array.apply(null, arguments).slice(1));
 }
 
+function fillPoly(color, points) {
+  global.context.strokeStyle = "";
+  global.context.fillStyle = color;
+  global.context.beginPath();
+  global.context.moveTo(points[0][0], points[0][1]);
+  for (var i = 1; i < points.length; i++) {
+    global.context.lineTo(points[i][0], points[i][1]);
+  }
+  global.context.closePath();
+  global.context.fill();
+}
+
 function drawLine() {
     assert(arguments.length == 5, "drawLine must take 5 arguments");
     global.context.strokeStyle = arguments[0];
@@ -97,10 +127,258 @@ function checkOverlap(a, b) {
              a.y + a.height < b.y || a.y > b.y + b.height);
 }
 
+function pointInRectangle(point, rect) {
+    return !(point[0] < rect.x || point[1] < rect.y || point[0] > rect.x + rect.width || point[1] > rect.y + rect.height);
+}
+
 function randomDouble(min, max) {
     return Math.random() * (max - min) + min;
 }
 
 function randomInt(min, max) {
     return Math.floor(randomDouble(min, max));
+}
+
+// Copy and pasted from https://github.com/substack/point-in-polygon/blob/master/index.js
+// Commit: e1888ce59696a5713b5520cbc17fa268ee703229 File: index.js
+// See License-substack.point-in-polygon (MIT)
+function pointInPolygon(point, vs) {
+    // ray-casting algorithm based on
+    // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+    var x = point[0], y = point[1];
+
+    var inside = false;
+    for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+        var xi = vs[i][0], yi = vs[i][1];
+        var xj = vs[j][0], yj = vs[j][1];
+
+        var intersect = ((yi > y) != (yj > y))
+            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) inside = !inside;
+    }
+
+    return inside;
+};
+
+const add = (a, b) => [a[0] + b[0], a[1] + b[1]];
+const sub = (a, b) => [a[0] - b[0], a[1] - b[1]];
+const mul = (a, s) => [a[0] * s, a[1] * s];
+const cmul = (a, s) => [a[0] * s[0], a[1] * s[1]];
+const div = (a, s) => [a[0] / s, a[1] / s];
+const dot = (a, b) => a[0] * b[0] + a[1] * b[1];
+const cross = (a, b) => a[0] * b[1] - a[1] * b[0];
+const sqnorm2d = (a) => dot(a, a);
+const norm2d = (a) => Math.sqrt(sqnorm2d(a));
+const comp = (v, basis) => dot(basis, v) / sqnorm2d(basis);
+const proj = (v, basis) => mul(basis, comp(v, basis));
+const lerpHelper = (va, vb, t) => va * (1 - t) + vb * t;
+const lerp = (a, b, t) => a instanceof Array ? zip(a, b, (va, vb) => lerpHelper(va, vb, t)) : lerpHelper(a, b, t);
+const rotate = (p, theta) => [Math.cos(theta) * p[0] - Math.sin(theta) * p[1], Math.sin(theta) * p[0] + Math.cos(theta) * p[1]];
+const sqdist = (a, b) => sqnorm2d(sub(a, b));
+const dist = (a, b) => Math.sqrt(sqdist(a, b));
+const atan2 = ([p1, p2]) => Math.atan2(p2[1] - p1[1], p2[0] - p1[0]);
+const perp = ([x, y]) => [-y, x];
+const normalize = v => div(v, norm2d(v));
+const flip = ([x, y]) => [-x, -y];
+
+const within = (val, l, r) => l <= val && val <= r;
+
+// Adapted from miyu/derp
+// NOTE: Assumes segments are valid (two distinct endpoints) NOT line-OVERLAPPING
+// that is, segments should not have more than 1 point of intersection.
+// if segments DO have more than 1 point of intersection, this returns no intersection found.
+function tryFindSegmentSegmentIntersectionT(a, b) {
+    // via http://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect
+    var p = a[0];
+    var r = sub(a[1], a[0])
+    var q = b[0];
+    var s = sub(b[1], b[0]);
+
+    var rxs = cross(r, s);
+    if (rxs == 0) {
+        return null;
+    }
+
+    var qmp = sub(q, p);
+    var t = cross(qmp, s) / rxs;
+    if (t < 0.0 || t > 1.0) {
+        return null;
+    }
+
+    var u = cross(qmp, r) / rxs;
+    if (u < 0.0 || u > 1.0) {
+        return null;
+    }
+
+    return t;
+}
+
+function tryFindSegmentSegmentIntersection(a, b) {
+    var t = tryFindSegmentSegmentIntersectionT(a, b);
+    if (!t) return null;
+    return [a[0][0] * t + a[0][1] * (1-t), a[1][0] * t + a[1][1] * (1-t)]
+}
+
+function tryFindSegmentPolyIntersectionT(a, b) {
+    var tIntersectMin = Infinity;
+    for (var i = 0; i < b.length - 1; i++) {
+        var t = tryFindSegmentSegmentIntersectionT(a, [b[i], b[i + 1]]);
+        if (t) tIntersectMin = Math.min(tIntersectMin, t)
+    }
+    return tIntersectMin == Infinity ? null : tIntersectMin;
+}
+
+function findSegmentPolyIntersectionTs(a, b) {
+    var tIntersects = [];
+    for (var i = 0; i < b.length - 1; i++) {
+        var t = tryFindSegmentSegmentIntersectionT(a, [b[i], b[i + 1]]);
+        if (t) tIntersects.push(t);
+    }
+    return tIntersects;
+}
+
+function tryFindSegmentPolyIntersection(a, b) {
+    var t = tryFindSegmentPolyIntersectionT(a, b);
+    if (!t) return null;
+    return lerp(a[0], a[1], t);
+}
+
+function findSegmentPolyIntersections(a, b) {
+    var ts = findSegmentPolyIntersectionTs(a, b);
+    return ts.map(t => lerp(a[0], a[1], t));
+}
+
+function findSegmentPolyIntersectionSegments(a, b) {
+    var results = [];
+    for (var i = 0; i < b.length - 1; i++) {
+        var segment = [b[i], b[i + 1]];
+        if (tryFindSegmentSegmentIntersectionT(a, segment)) {
+            results.push(segment);
+        }
+    }
+    return results;
+}
+
+function findNearestPointLineT(query, line) {
+    var [p1, p2] = line;
+    var p1p2 = sub(p2, p1);
+    var p1Query = sub(query, p1);
+    var p1QueryProjP1P2Component = comp(p1Query, p1p2);
+    return p1QueryProjP1P2Component;
+}
+
+function findNearestPointSegmentT(query, segment) {
+    var p1QueryProjP1P2Component = findNearestPointLineT(query, segment);
+    return Math.max(0, Math.min(1, p1QueryProjP1P2Component));
+}
+
+function findNearestPointSegment(query, segment) {
+    var t = findNearestPointSegmentT(query, segment);
+    return lerp(segment[0], segment[1], t);
+}
+
+function findNearestPointPoly(p, poly) {
+    var dNearestMin = Infinity;
+    var pNearest = null;
+    var segNearest = null;
+    var segNearestIndex = -1;
+    for (var i = 0; i < poly.length - 1; i++) {
+        var seg = [poly[i], poly[i + 1]];
+        var nearestPoint = findNearestPointSegment(p, seg);
+        var d = dist(p, nearestPoint);
+        if (d < dNearestMin) {
+            dNearestMin = d;
+            pNearest = nearestPoint;
+            segNearest = seg;
+            segNearestIndex = i;
+        }
+    }
+    return [dNearestMin, pNearest, segNearest, segNearestIndex];
+}
+
+function findRectangleLineIntersections(rect, seg) {
+    var s1 = [[rect.x, rect.y], [rect.x + rect.width, rect.y]];
+    var s2 = [[rect.x, rect.y + rect.height], [rect.x + rect.width, rect.y + rect.height]];
+    var s3 = [[rect.x, rect.y], [rect.x, rect.y + rect.height]];
+    var s4 = [[rect.x + rect.width, rect.y], [rect.x + rect.width, rect.y + rect.height]];
+
+    return [
+        tryFindSegmentSegmentIntersection(s1, seg),
+        tryFindSegmentSegmentIntersection(s2, seg),
+        tryFindSegmentSegmentIntersection(s3, seg),
+        tryFindSegmentSegmentIntersection(s4, seg)
+    ].filter(val => val);
+}
+
+function findRectanglePolyIntersectionPoints(rect, poly) {
+    var s1 = [[rect.x, rect.y], [rect.x + rect.width, rect.y]];
+    var s2 = [[rect.x, rect.y + rect.height], [rect.x + rect.width, rect.y + rect.height]];
+    var s3 = [[rect.x, rect.y], [rect.x, rect.y + rect.height]];
+    var s4 = [[rect.x + rect.width, rect.y], [rect.x + rect.width, rect.y + rect.height]];
+    return [].concat(
+        findSegmentPolyIntersections(s1, poly),
+        findSegmentPolyIntersections(s2, poly),
+        findSegmentPolyIntersections(s3, poly),
+        findSegmentPolyIntersections(s4, poly));
+}
+
+function findRectanglePolyIntersectionSegments(rect, poly) {
+    var s1 = [[rect.x, rect.y], [rect.x + rect.width, rect.y]];
+    var s2 = [[rect.x, rect.y + rect.height], [rect.x + rect.width, rect.y + rect.height]];
+    var s3 = [[rect.x, rect.y], [rect.x, rect.y + rect.height]];
+    var s4 = [[rect.x + rect.width, rect.y], [rect.x + rect.width, rect.y + rect.height]];
+    
+    var results = [];
+    for (let i = 0; i < poly.length - 1; i++) {
+        var segment = [poly[i], poly[i + 1]];
+        if (tryFindSegmentSegmentIntersectionT(s1, segment) ||
+            tryFindSegmentSegmentIntersectionT(s2, segment) ||
+            tryFindSegmentSegmentIntersectionT(s3, segment) ||
+            tryFindSegmentSegmentIntersectionT(s4, segment) ||
+            pointInRectangle(segment[0], rect)
+        ) {
+            results.push(segment);
+        }
+    }
+    return results;
+}
+
+//-------------------------------------------------------------------------------------------------
+function triwave(x) {
+    x *= 4 / (2 * Math.PI); // from 4-period to 2pi period
+    x = ((x % 4.0) + 4.0) % 4.0;
+    if (x < 1) return x;
+    if (x > 3) return x - 4;
+    return 2 - x;
+}
+
+function mapMany(arr, map) {
+    return arr.reduce((acc, it) => acc.concat(map(it)), []);
+}
+
+function enumerate(arrOrN) {
+    if (typeof (arrOrN) === 'number') {
+        var arr = [];
+        for (var i = 0; i < arrOrN; i++) {
+            arr.push(i);
+        }
+        return arr;
+    } else {
+        return arr.map((val, i) => [i, val]);
+    }
+}
+
+function any(arr, pred) {
+    for (let i = 0; i < arr.length; i++) {
+        if (pred(arr[i])) return true;
+    }
+    return false;
+}
+
+function zip(a1, a2, zipper) {
+    return a1.map((val, i) => zipper(val, a2[i]));
+}
+
+function arrayify(val) {
+    return val instanceof Array ? val : [val];
 }
--- program.js
+++ program.js
@@ -1,91 +1,178 @@
+// ReSharper disable AssignmentInConditionExpression, CoercedEqualsUsing
+const makeTPoly = (w, h, t) => [
+    [-w / 2, -t / 2],
+    [-t / 2, -t / 2],
+    [-t / 2, -h / 2],
+    [t / 2, -h / 2],
+    [t / 2, -t / 2],
+    [w / 2, -t / 2],
+    [w / 2, t / 2],
+    [t / 2, t / 2],
+    [t / 2, h / 2],
+    [-t / 2, h / 2],
+    [-t / 2, t / 2],
+    [-w / 2, t / 2],
+    [-w / 2, -t / 2]
+]; // ==||== shaped
+
+const makeDiamondPoly = (w, h) => [
+    [-w / 2, 0],
+    [0, -h / 2],
+    [w / 2, 0],
+    [0, h / 2],
+    [-w / 2, 0]
+];
+
+const makeEllipsePoly = (w, h, n) => enumerate(n + 1).map(i => cmul(rotate([1, 0], 2 * Math.PI * i / n), [w / 2, h / 2]));
+
+const scalePoly = (poly, sx, sy) => poly.map(([px, py]) => [px * sx, py * (sy || sx)]);
+const translatePoly = (poly, x, y) => poly.map(([px, py]) => [px + x, py + y]);
+const rotatePoly = (poly, theta) => poly.map(p => rotate(p, theta));
+
 window.onload = function() {
     init(800, 600);
 
-    var gridSideLength = 30;
-    var gridDisplayTileLength = 16;
-    var gridDisplayTileSpacing = 18;
+    const initialPlayerPosition = () => [200, 200];
+    var playerPosition = initialPlayerPosition();
+    var playerVelocity = [0, 0];
+    var playerWidth = 10 * 1.01; // slightly bigger so never perfectly aligns with integer-sized segments, lol
+    var playerHeight = 16 * 1.01;
+    var tJumpAllowedUntil = -1;
 
-    var grid = [];
-    for (var y = 0; y < gridSideLength; y++) {
-        var row = [];
-        for (var x = 0; x < gridSideLength; x++) {
-            row.push(0);
+    var getPlayerRectangle = (ppos) => makeRectangle((ppos || playerPosition)[0] - playerWidth / 2, (ppos || playerPosition)[1] - playerHeight, playerWidth, playerHeight);
+    var bufferRectangle = (rect, bw, bh) => makeRectangle(rect.x - bw / 2, rect.y - bh / 2, rect.width + bw, rect.height + bh);
+    var crossPosition = [200, 300];
+    var crossAngle = 0;
+    var crossAngularVelocity = 0;
+
+    // todo: find intersections with segments, not points. Then can resolve square butt case.
+    var resolveCollision = (unitPosition, unitVelocity, getUnitRectangle, polys) => {
+        const computeIntersections = () => mapMany(polys, poly => findRectanglePolyIntersectionSegments(getUnitRectangle(unitPosition), poly).map(val => [poly, val]));
+        let intersections = null;
+        while ((intersections = computeIntersections()).length != 0) {
+            const [positionOffset, aboveness] = intersections.reduce(([positionOffsetAcc, yclampdir], [poly, seg]) => {
+                const angle = atan2(seg) * 180 / Math.PI;
+                const vseg = sub(seg[1], seg[0]);
+                const vsegPerpUnitFlip = flip(normalize(perp(vseg)));
+                const verticalEpsilon = 10; // steepeness in which climbing becomes increasingly hard til impossible
+                const isVertical = within(angle, 90 - verticalEpsilon, 90 + verticalEpsilon) || within(angle, -90 - verticalEpsilon, -90 + verticalEpsilon);
+                const isAbove = Math.abs(angle) < 90;
+                return [add(positionOffsetAcc, vsegPerpUnitFlip), isAbove && !isVertical ? yclampdir + 1 : yclampdir - 1];
+            }, [[0, 0], 0]);
+            unitPosition = add(unitPosition, positionOffset);
+            if (aboveness > 0) {
+                unitVelocity[1] = Math.min(0, unitVelocity[1]);
+            } else if (aboveness < 0) {
+                unitVelocity[1] = Math.max(0, unitVelocity[1]);
+            }
         }
-        grid.push(row);
-    }
+        return [unitPosition, unitVelocity];
+    };
 
-    var snakeLength = 3;
-    var snakeX = 3, snakeY = 4;
-    var snakeVx = 1, snakeVy = 0;
-    var snakeLengthIncrease = 1;
-
-    var appleX = 7, appleY = 7;
-
-    var stepTimeCounter = 0;
-    var stepTimeThreshold = 0.05;
-
-    onFrameEnter(dt => {
-        // Always handle input
-        if (Key.isDown('ArrowLeft')) {
-            snakeVx = -1;
-            snakeVy = 0;
+    var shiftUnit = (unitPosition, unitVelocity, offset, getUnitRectangle, polys) => {
+        if (offset[0] == 0 && offset[1] == 0) {
+            return [unitPosition, unitVelocity];
         }
-        if (Key.isDown('ArrowRight')) {
-            snakeVx = 1;
-            snakeVy = 0;
+        const direction = normalize(offset);
+        const length = norm2d(offset);
+        const steps = Math.min(32, Math.ceil(length));
+        for (let i = 0; i < length; i++) {
+            const desiredPosition = add(unitPosition, mul(direction, length / steps));
+            const initialVelocity = unitVelocity;
+            [unitPosition, unitVelocity] = resolveCollision(desiredPosition, unitVelocity, p => getUnitRectangle(p), polys);
         }
-        if (Key.isDown('ArrowUp')) {
-            snakeVx = 0;
-            snakeVy = -1;
-        }
-        if (Key.isDown('ArrowDown')) {
-            snakeVx = 0;
-            snakeVy = 1;
+        return [unitPosition, unitVelocity];
+    };
+
+    const createCrossPlatformPoly = () => translatePoly(rotatePoly(makeTPoly(200, 40, 10), crossAngle), crossPosition[0], crossPosition[1]);
+
+    const defaultComputePolys = (t) => [
+        createCrossPlatformPoly(),
+        translatePoly(rotatePoly(makeDiamondPoly(200, 40), (t / 20) * 2 * Math.PI), 500 + 100 * triwave((t / 3) * 2 * Math.PI), 400),
+        translatePoly(rotatePoly(makeEllipsePoly(200, 40, 10), Math.sin((t / 4) * 2 * Math.PI) * 0.1 * 2 * Math.PI), 400 + 100 * Math.sin((t / 5) * 2 * Math.PI), 200)
+    ];
+
+    var ta = document.createElement('textarea');
+    ta.style.width = '800px';
+    ta.style.height = '400px';
+    ta.value = defaultComputePolys.toString();
+    var refresh = document.createElement('button');
+    refresh.innerText = 'refresh';
+    document.body.appendChild(ta);
+    document.body.appendChild(refresh);
+
+    const computePolys = t => eval('(' + ta.value + ')')(t);
+
+    onTickEnter((dt, t) => {
+        // r => reset player position
+        if (Key.isDown('r')) {
+            playerPosition = initialPlayerPosition();
+            playerVelocity = [0, 0];
         }
 
-        // Only draw / advance snake if step threshold passed
-        stepTimeCounter += dt;
-        if (stepTimeCounter < 0) return;
-        stepTimeCounter = -stepTimeThreshold;
+        // compute map geometry
+        const polys = computePolys(t);
 
-        snakeX += snakeVx;
-        snakeY += snakeVy;
-        if (grid[snakeY][snakeX] != 0) {
-            throw "gg";
+        // resolve geometry/player intersection
+        [playerPosition, playerVelocity] = resolveCollision(playerPosition, playerVelocity, getPlayerRectangle, polys);
+
+        // move player (gravity/velocity, arrow keys)
+        [playerPosition, playerVelocity] = shiftUnit(playerPosition, playerVelocity, mul(playerVelocity, dt), getPlayerRectangle, polys);
+
+        // torque the cross/t-platform from above
+        {
+            const tPoly = createCrossPlatformPoly();
+            const belowRect = getPlayerRectangle(add(playerPosition, [0, 4]));
+            const intersections = findRectanglePolyIntersectionPoints(belowRect, tPoly);
+            if (intersections.length > 0) {
+                var torquePoint = div(intersections.reduce(([ax, ay], [px, py]) => [ax + px, ay + py], [0, 0]), intersections.length);
+                var r = sub(torquePoint, crossPosition);
+                var f = [0, 0.1];
+                var torque = cross(r, f);
+                // torque = i alpha => alpha = torque for i = 1
+                crossAngularVelocity += torque * dt;
+            }
         }
-        grid[snakeY][snakeX] = snakeLength;
+        crossAngle += crossAngularVelocity * dt;
+        var avKeptPerSecond = 0.05;
+        var avKeptPerFrame = Math.pow(avKeptPerSecond, 1 / global.tickRate); // // 0.8 = r ^ 60
+        crossAngularVelocity *= avKeptPerFrame;
 
-        while (grid[appleY][appleX] > 0) {
-            appleX = randomInt(0, gridSideLength);
-            appleY = randomInt(0, gridSideLength);
-            snakeLength += snakeLengthIncrease;
-            for (var y = 0; y < gridSideLength; y++) {
-                for (var x = 0; x < gridSideLength; x++) {
-                    if (grid[y][x] > 0) {
-                        grid[y][x] += snakeLengthIncrease;
-                    }
+        // jump logic
+        const testRect = getPlayerRectangle(add(playerPosition, [0, 2]));
+        if (any(polys, poly => findRectanglePolyIntersectionPoints(testRect, poly).length > 0)) {
+            tJumpAllowedUntil = t + 0.3;
+        } else {
+            // gravity
+            playerVelocity[1] += 500 * dt;
+        }
+        if (Key.isDown('space') && t < tJumpAllowedUntil) {
+            playerVelocity[1] = -300;
+            tJumpAllowedUntil = t;
+
+            {
+                const tPoly = createCrossPlatformPoly();
+                const belowRect = getPlayerRectangle(add(playerPosition, [0, 2]));
+                const intersections = findRectanglePolyIntersectionPoints(belowRect, tPoly);
+                if (intersections.length > 0) {
+                    var torquePoint = div(intersections.reduce(([ax, ay], [px, py]) => [ax + px, ay + py], [0, 0]), intersections.length);
+                    var r = sub(torquePoint, crossPosition);
+                    var f = [0, 1];
+                    var torque = cross(r, f);
+                    // torque = i alpha => alpha = torque for i = 1
+                    crossAngularVelocity += torque * dt;
                 }
             }
         }
 
-        clear("#000000");
-        for (var y = 0; y < gridSideLength; y++) {
-            for (var x = 0; x < gridSideLength; x++) {
-                var cellRectangle = makeRectangle(
-                    x * gridDisplayTileSpacing + 5,
-                    y * gridDisplayTileSpacing + 5,
-                    gridDisplayTileLength,
-                    gridDisplayTileLength);
+        // left/right motion
+        var motionOffset = Key.isDown('ArrowLeft') ? [-200 * dt, 0] : (Key.isDown('ArrowRight') ? [200 * dt, 0] : [0, 0]);
+        [playerPosition] = shiftUnit(playerPosition, playerVelocity, motionOffset, getPlayerRectangle, polys);
+    });
 
-                if (grid[y][x] > 0) {
-                    fillRect("#00FF00", cellRectangle);
-                    grid[y][x]--;
-                } else if (x == appleX && y == appleY) {
-                    fillRect("#FF0000", cellRectangle);
-                } else {
-                    fillRect("#333333", cellRectangle);
-                }
-            }
-        }
+    onFrameEnter((dt, t) => {
+        clear('#00ff00');
+        fillRect('#FF0000', getPlayerRectangle());
+        computePolys(t).forEach(poly => fillPoly('#FF00FF', poly));
     });
 };
